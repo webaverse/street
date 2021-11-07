c357047d171e2e70bc21d97c8b46bab51be4d429
yaser <yaser@superblacksoftware.net>
2021-11-07 23:57:17 +0000

    remove extra letters in filename

--- logdepthbuf/index.js
+++ logdepthbuf/index.js
@@ -1,6 +1,6 @@
-export {default as logdepthbuf_fragmentGlsl} from "./logdepthbuf_fragment.glsl";
-export {default as logdepthbuf_pars_fragmentGlsl} from "./logdepthbuf_pars_fragment.glsl";
+export {default as logdepthbuf_fragmentGlsl} from "./logdepthbuf_fragment";
+export {default as logdepthbuf_pars_fragmentGlsl} from "./logdepthbuf_pars_fragment";
 
 
-export {default as logdepthbuf_vertexGlsl} from "./logdepthbuf_vertex.glsl";
-export {default as logdepthbuf_pars_vertexGlsl} from "./logdepthbuf_pars_vertex.glsl";
+export {default as logdepthbuf_vertexGlsl} from "./logdepthbuf_vertex";
+export {default as logdepthbuf_pars_vertexGlsl} from "./logdepthbuf_pars_vertex";
