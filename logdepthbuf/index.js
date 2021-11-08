4c0198071108b7c1335cc663ba95ff01552a0913
yaser <yaser@superblacksoftware.net>
2021-11-08 00:07:57 +0000

    import changes

--- logdepthbuf/index.js
+++ logdepthbuf/index.js
@@ -1,6 +1,6 @@
-export {default as logdepthbuf_fragmentGlsl} from "./logdepthbuf_fragment";
-export {default as logdepthbuf_pars_fragmentGlsl} from "./logdepthbuf_pars_fragment";
+export {default as logdepthbuf_fragmentGlsl} from "./logdepthbuf_fragment.js";
+export {default as logdepthbuf_pars_fragmentGlsl} from "./logdepthbuf_pars_fragment.js";
 
 
-export {default as logdepthbuf_vertexGlsl} from "./logdepthbuf_vertex";
-export {default as logdepthbuf_pars_vertexGlsl} from "./logdepthbuf_pars_vertex";
+export {default as logdepthbuf_vertexGlsl} from "./logdepthbuf_vertex.js";
+export {default as logdepthbuf_pars_vertexGlsl} from "./logdepthbuf_pars_vertex.js";
