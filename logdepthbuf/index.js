b2505a567f316a69ffbb3b34746575080f330055
yaser <yaser@superblacksoftware.net>
2021-11-09 12:39:17 +0000

    remove unnecessary files

--- logdepthbuf/index.js
+++
@@ -1,6 +0,0 @@
-export {default as logdepthbuf_fragmentGlsl} from "./logdepthbuf_fragment.js";
-export {default as logdepthbuf_pars_fragmentGlsl} from "./logdepthbuf_pars_fragment.js";
-
-
-export {default as logdepthbuf_vertexGlsl} from "./logdepthbuf_vertex.js";
-export {default as logdepthbuf_pars_vertexGlsl} from "./logdepthbuf_pars_vertex.js";
--- logdepthbuf/logdepthbuf_fragment.js
+++
@@ -1,7 +0,0 @@
-export default /* glsl */`
-#if defined( USE_LOGDEPTHBUF ) && defined( USE_LOGDEPTHBUF_EXT )
-	// Doing a strict comparison with == 1.0 can cause noise artifacts
-	// on some platforms. See issue #17623.
-	gl_FragDepthEXT = log2( vFragDepth ) * logDepthBufFC * 0.5;
-#endif
-`;
